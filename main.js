c827f4b8d03e6f1a52b234ac4f104c6571c97993
jake497726 <jung.jin@northeastern.edu>
2023-02-14 12:32:57 -0500

    Create main.js

---
+++ main.js
@@ -0,0 +1,26 @@
+// Create list of points
+let points = document.getElementsByTagName("circle");
+
+// loop through all points
+for (let i = 0; i < points.length; i++) {
+    
+    // check each point for clicks
+    let point = points[i];
+    point.addEventListener("click", function(){
+
+                  // click then add border
+                  this.classList.toggle("Border");
+
+                  //find the cx and cy
+                  let cx = this.getAttribute("cx") / 50;
+                  let cy = (500 - this.getAttribute("cy")) / 50;
+
+                  // create new text
+                  let text1 = "Last point clicked: "
+                  let text2 = "(" + cx +"," + cy + ")"
+  
+                  // show the text information
+                  document.getElementById("text1").innerHTML = text1;
+                  document.getElementById("text2").innerHTML = text2;
+}
+)}
